e8fc5eddebcd0d09e61cd79ecee5da4cfc1405f2
cjsepol <cjsepulveda@ucundinamarca.edu.co>
2021-10-03 22:37:55 -0500

    Añadir video

---
+++ Estructuras/src/main/java/co/edu/ucundinamarca/estructuras/Libro.java
@@ -0,0 +1,33 @@
+package co.edu.ucundinamarca.estructuras;
+
+public class Libro {
+  private String titulo;
+  private String autor;
+  private String isbn;
+
+  public Libro(String titulo, String autor, String isbn){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.isbn = isbn;
+  }
+  public String getTitulo(){
+    return titulo;
+  }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor(){
+      return autor;
+    }
+    public void setAutor(){
+      this.autor = autor;
+    }
+    public String getIsbn(){
+      return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
---
+++ Estructuras/src/main/java/co/edu/ucundinamarca/estructuras/Main.java
@@ -0,0 +1,21 @@
+
+package co.edu.ucundinamarca.estructuras;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Clase Principal
+ * @author handresortiz
+ */
+public class Main {
+
+    static Logger log = Logger.getLogger(Lista.class.getName());
+
+    public static void main(String[] args) {
+
+        log.debug("Hello World : this is a debug message");
+        log.info("Hello World : this is an info message");
+
+    }
+
+}
